3e4032ab9b5a1a5a393216997130e60f012fc4fd
Raxita Parghi <parghiraxita03@gmail.com>
2026-08-12 18:01:34 +0530

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,9 @@
+# Data Types
+name = "Raxita" #string
+age = 19 #Integer
+percentage = 80.5 #Float
+is_student = True #Boolean
+print(type(name))
+print(type(age))
+print(type(percentage))
+print(type(is_student))
